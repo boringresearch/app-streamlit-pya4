3ae80fc9e7ffb6f7b20f77af67cc01c2b2c546a7
xihajun <www.junfan.com@gmail.com>
2022-02-09 14:33:38 +0000

    Update app.py

--- app.py
+++ app.py
@@ -24,11 +24,20 @@ height = st.sidebar.slider('height inches', 0.01, 15.00, 8.27)  # min, max, defa
 st.subheader('A4')
 
 fig = plt.figure(figsize=(8.27,11.69))
-ax = fig.add_subplot(221)
-ax2 = fig.add_subplot(222)
+ax = fig.add_subplot(321)
+ax2 = fig.add_subplot(322)
+ax3 = fig.add_subplot(323)
+ax4 = fig.add_subplot(324)
+ax5 = fig.add_subplot(325)
+ax6 = fig.add_subplot(326)
+
 x = np.linspace(0,4,1000)
 ax.plot(x, np.sin(x))
 ax2.plot(x, np.cos(x), 'r:')
+ax3.plot(x, np.sin(x))
+ax4.plot(x, np.cos(x), 'g:')
+ax5.plot(x, np.sin(x))
+ax6.plot(x, np.cos(x), 'b:')
 st.pyplot(fig, clear_figure=True)
 
 
