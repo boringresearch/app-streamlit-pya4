639e9b2ed682069ef6dbc60dcc7cdd1a358f98cf
xihajun <www.junfan.com@gmail.com>
2022-02-09 14:26:08 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,6 @@ apptitle = 'A4 simulation'
 
 st.set_page_config(page_title=apptitle, page_icon=":eyeglasses:")
 
-# -- Default detector list
-detectorlist = ['H1','L1', 'V1']
-
 # Title the app
 st.title('A4 Simulation')
 
@@ -18,11 +15,10 @@ st.sidebar.markdown("## Select the page size")
 
 # -- Create sidebar for plot controls
 st.sidebar.markdown('## Set figure width')
-width = st.sidebar.slider('width inches', 0.1, 15, 11.69)  # min, max, default
+width = st.sidebar.slider('width inches', 0.01, 15.00, 11.69)  # min, max, default
 
 st.sidebar.markdown('## Set figure height')
-height = st.sidebar.slider('height inches', 0.1, 15, 8.27)  # min, max, default
-
+height = st.sidebar.slider('height inches', 0.01, 15.00, 8.27)  # min, max, default
 
 
 st.subheader('A4')
