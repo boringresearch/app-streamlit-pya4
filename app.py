8c4dfa4c5b6714639a0263024ef720af6646b534
xihajun <www.junfan.com@gmail.com>
2022-02-09 14:30:30 +0000

    Update app.py

--- app.py
+++ app.py
@@ -21,12 +21,18 @@ st.sidebar.markdown('## Set figure height')
 height = st.sidebar.slider('height inches', 0.01, 15.00, 8.27)  # min, max, default
 
 
+f = plt.figure(figsize=(10,3))
+
+
 st.subheader('A4')
 
-fig = plt.figure(figsize=(11.69,8.27))
+fig = plt.figure(figsize=(8.27,11.69))
 st.pyplot(fig, clear_figure=True)
+ax = fig.add_subplot(121)
+x = np.linspace(0,4,1000)
+ax.plot(x, np.sin(x))
+ax2.plot(x, np.cos(x), 'r:')
 
-
-fig = plt.figure(figsize=(width,height))
+fig = plt.figure(figsize=(height,width))
 st.pyplot(fig, clear_figure=True)
 
